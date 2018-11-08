88895ea13dbe093aa662c94c91c852cd8bdf3ef9
turner-prize <hiphoperation@hotmail.com>
2018-11-08 21:16:42 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def hello():
-    return 'post request success'
+    data = request.data
+    return data
