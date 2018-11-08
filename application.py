42ea83698f2193d1ffeaf7ff8141bc786da8658b
turner-prize <hiphoperation@hotmail.com>
2018-11-08 21:15:11 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,7 @@
 from flask import Flask
-import requests
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def hello():
-    r = requests.get("https://fantasy.premierleague.com/drf/elements/")
-    r = r.json()
-    return r[3]
+    return 'post request success'
